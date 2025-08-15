772eaf0873d6a2420ccb0934fa7ac9d168fbc818
Onur Kaan Şen <onurkaan3888@gmail.com>
2025-08-15 19:19:40 +0300

    feat: add loan record entity

---
+++ Loan.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+class  Loan {
+    String userId;
+    String isbn;
+    Date dueDate;
+
+    public Loan(String userId, String isbn, Date dueDate) {
+        this.userId = userId;
+        this.isbn = isbn;
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User ID: " + userId + ", ISBN: " + isbn + ", Due Date: " + dueDate;
+    }
+}
